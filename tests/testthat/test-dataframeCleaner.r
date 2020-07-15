dd6b89f758baf73020c4191d6593a843c7e4590a
Laurens van der Burg <laurens@ggbo.eu>
2020-07-15 23:08:55 +0200

    minor bugfix in test

--- tests/testthat/test-dataframeCleaner.r
+++ tests/testthat/test-dataframeCleaner.r
@@ -3,6 +3,7 @@ test_that("make.true.NA_df works", {
   dfr <- data.frame( A = c(1,2,NaN,3)
                    , B = c("a", "<NA>" ,"","d")
                    , C = c(as.Date("2012-01-01"), as.Date("2012-01-02"), as.Date("2012-01-03"), as.Date("2012-01-04"))
+                   , stringsAsFactors = FALSE
                    )
   test_outcome <- make.true.NA_df(dfr)
   correct_outcome <- structure(list( A = c(1, 2, NA, 3)
