bcb0056268a7cd9ab2faed00f2445934af361ae3
GreenGrassBlueOcean <43173895+GreenGrassBlueOcean@users.noreply.github.com>
2021-02-12 15:55:33 +0100

    added installeikon to github workflow

--- R/refinitiv.r
+++ R/refinitiv.r
@@ -50,9 +50,9 @@ install_eikon <- function(method = "auto", conda = "auto", envname= "r-reticulat
   # reticulate::use_condaenv(condaenv = envname, conda = conda)
 
   if (!reticulate::py_module_available("eikon") || update ) {
-    try(reticulate::conda_remove(packages = c("numpy", "pandas", "eikon") , envname = envname,  conda = conda ))
+    try(reticulate::conda_remove(packages = c("numpy", "pandas", "nest-asyncio","eikon") , envname = envname,  conda = conda ))
     # reticulate::py_install(packages = c("httpx==0.14.2", "numpy", "eikon==1.1.6post2") , envname = envname,  method = method, conda = conda, pip = TRUE )
-    reticulate::py_install(packages = c("numpy==1.19.3", "pandas","eikon") , envname = envname,  method = method, conda = conda, pip = TRUE )
+    reticulate::py_install(packages = c("numpy==1.19.3", "pandas", "nest-asyncio==1.3.0","eikon") , envname = envname,  method = method, conda = conda, pip = TRUE )
   }
 
   return("Eikon Python interface successfully installed or updated")
---
+++ tests/testthat/test-install_eikon.R
@@ -0,0 +1,3 @@
+test_that("install_eikon works", {
+    expect_error(install_eikon(), NA)
+})
