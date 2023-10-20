e69134bc82d420bc3ff21e81d9d79b5501094928
Laurens van der burg <laurens@ggbo.eu>
2023-10-20 11:28:38 +0200

    Update test-GetHistoricalPricing.R

--- tests/testthat/test-GetHistoricalPricing.R
+++ tests/testthat/test-GetHistoricalPricing.R
@@ -4,7 +4,7 @@ test_that("stops when it has the wrong eikonobject", {
   options(.RefinitivPyModuleName = "wrong object")
 
     expect_error( rd_GetHistoricalPricing(RDObject = NULL)
-                , "historical pricing is only available when JSON --> RefinitivJsonConnect() or Python Refinitiv data --> RDConnect() is used as EikonObject"
+                , "historical pricing is only available when JSON --> RefinitivJsonConnect() or Python Refinitiv data --> RDConnect() is used as RDObject"
                 , fixed = TRUE)
 
   options(.RefinitivPyModuleName = OldModuleName)
@@ -43,5 +43,78 @@ test_that("historical pricing delivers identical results", {
                                        , count = 20L, RDObject = RefinitivJsonConnect())
 
   expect_equal(Vodafone, Vodafone_json)
+
+})
+
+
+test_that("historical pricing delivers identical intraday results", {
+
+  testthat::skip_if(is.null(getOption(".EikonApiKey")))
+
+  intraday <- rd_GetHistoricalPricing( universe = c("VOD.L", "AAPL.O")
+                                     , interval = "PT1M", count = 20L
+                                     , sessions= c("pre","normal","post")
+                                     , RDObject = RDConnect())
+
+  intraday_json <- rd_GetHistoricalPricing( universe = c("VOD.L", "AAPL.O")
+                                       , interval = "PT1M", count = 20L
+                                       , sessions= c("pre","normal","post")
+                                       , RDObject = RefinitivJsonConnect())
+
+  expect_equal(intraday, intraday_json)
+
+  expect_is(intraday, 'data.frame')
+  expect_equal(nrow(intraday), 40L)
+
+  expect_equal(lapply(intraday,class)
+               ,list(universe = "character", DATE_TIME = "character", HIGH_1 = "numeric",
+                     LOW_1 = "numeric", OPEN_PRC = "numeric", TRDPRC_1 = "numeric",
+                     NUM_MOVES = "integer", ACVOL_UNS = "integer", HIGH_YLD = "logical",
+                     LOW_YLD = "logical", OPEN_YLD = "logical", YIELD = "logical",
+                     BID_HIGH_1 = "numeric", BID_LOW_1 = "numeric", OPEN_BID = "numeric",
+                     BID = "numeric", BID_NUMMOV = "integer", ASK_HIGH_1 = "numeric",
+                     ASK_LOW_1 = "numeric", OPEN_ASK = "numeric", ASK = "numeric",
+                     ASK_NUMMOV = "integer", MID_HIGH = "numeric", MID_LOW = "numeric",
+                     MID_OPEN = "numeric", MID_PRICE = "numeric"))
+
+
+
+  expect_equal(sort(unique(intraday$universe)), c("AAPL.O", "VOD.L"))
+
+})
+
+
+test_that("historical pricing delivers identical interday results", {
+
+  testthat::skip_if(is.null(getOption(".EikonApiKey")))
+
+
+  interday <- rd_GetHistoricalPricing(universe = c("VOD.L", "AAPL.O"), interval = "P1D", count = 20L
+                                     , fields =c("BID","ASK","OPEN_PRC","HIGH_1","LOW_1","TRDPRC_1","NUM_MOVES","TRNOVR_UNS") )
+
+  interday_json <- rd_GetHistoricalPricing(universe = c("VOD.L", "AAPL.O"), interval = "P1D", count = 20L
+                                           , fields =c("BID","ASK","OPEN_PRC","HIGH_1","LOW_1","TRDPRC_1","NUM_MOVES","TRNOVR_UNS")
+                                           , RDObject = RefinitivJsonConnect())
+
+
+  expect_equal(interday, interday_json)
+
+  expect_is(interday, 'data.frame')
+  expect_equal(nrow(interday), 40L)
+
+  expect_equal(lapply(interday,class)
+               ,list(universe = "character", DATE_TIME = "character", HIGH_1 = "numeric",
+                     LOW_1 = "numeric", OPEN_PRC = "numeric", TRDPRC_1 = "numeric",
+                     NUM_MOVES = "integer", ACVOL_UNS = "integer", HIGH_YLD = "logical",
+                     LOW_YLD = "logical", OPEN_YLD = "logical", YIELD = "logical",
+                     BID_HIGH_1 = "numeric", BID_LOW_1 = "numeric", OPEN_BID = "numeric",
+                     BID = "numeric", BID_NUMMOV = "integer", ASK_HIGH_1 = "numeric",
+                     ASK_LOW_1 = "numeric", OPEN_ASK = "numeric", ASK = "numeric",
+                     ASK_NUMMOV = "integer", MID_HIGH = "numeric", MID_LOW = "numeric",
+                     MID_OPEN = "numeric", MID_PRICE = "numeric"))
+
+
+
+  expect_equal(sort(unique(interday$universe)), c("AAPL.O", "VOD.L"))
 
 })
